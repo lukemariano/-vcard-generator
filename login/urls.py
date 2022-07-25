4a6b0af0f2fcea2ffe7cf75656e72834adfa0d4b
Lucas Mariano <lucas.svmariano@gmail.com>
2022-07-25 01:00:00 -0300

    login implemented

--- login/urls.py
+++ login/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='login.index')
+    path('', views.index, name='login.index'),
+    path('login/', views.login_view, name='login.login_view'),
+    path('login_auth/', views.login_auth, name="login.login_auth"),
+    path('loggin/vcard', views.vcard, name="login.vcard"),
 ]
--- login/views.py
+++ login/views.py
@@ -1,8 +1,31 @@
 # from django.http import HttpResponse
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect, render
 
 # Create your views here.
 
 
 def index(request):
     return render(request, 'login/index.html')
+
+
+def login_view(request):
+    return render(request, 'login/login.html')
+
+
+def login_auth(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('login.vcard')
+        else:
+            return render(request, 'login/login.html', {
+                'message': "Invalid Credentials",
+            })
+
+
+def vcard(request):
+    return render(request, 'login/vcard.html')
